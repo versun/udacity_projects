aa74fc18731d8287f27d405adf77bcedd2cf9762
versun <versun.pan@gmail.com>
2019-01-07 19:31:24 +0800

    add dermatologist-ai

---
+++ 2-机器学习工程师/D-深度学习/卷积神经网络/conv.py
@@ -0,0 +1,13 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+model = Sequential()
+model.add(Conv2D(filters=16, kernel_size=2, padding='same', activation='relu', input_shape=(32, 32, 3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64, kernel_size=2, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(500, activation='relu'))
+model.add(Dense(10, activation='softmax'))
